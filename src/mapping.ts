2a273299f3b1ffca8399c3eebaf0452001d54912
benesjan <janbenes1234@gmail.com>
2020-09-24 15:59:17 +0200

    Initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,13 @@
+import {LRCRewarded, LRCStaked, LRCWithdrawn} from "../generated/UserStakingPool/UserStakingPool";
+
+export function handleLRCRewarded(event: LRCRewarded): void {
+
+}
+
+export function handleLRCStaked(event: LRCStaked): void {
+
+}
+
+export function handleLRCWithdrawn(event: LRCWithdrawn): void {
+
+}
