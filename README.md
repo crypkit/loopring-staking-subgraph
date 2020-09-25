71bd216124b5c396dc6f1434c9d8951d59f7bf52
benesjan <janbenes1234@gmail.com>
2020-09-25 12:12:11 +0200

    Updated README.md, bug fixes

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Loopring Staking Subgraph
 
-Subgraph indexing LRCRewarded, LRCStaked and LRCWithdrawn events
+A subgraph indexing the following events on UserStakingPool [contract](https://etherscan.io/address/0xF4662bB1C4831fD411a95b8050B3A5998d8A4A5b#code): 
+- LRCStaked(address indexed user, uint amount)
+- LRCWithdrawn(address indexed user, uint amount)
+- LRCRewarded(address indexed user, uint amount)
+
+### Compilation and deployment
+
+1. Install the dependencies:
+    ```bash
+    npm install
+    ```
+2. Generate the types:
+    ```bash
+    npm run codegen
+    ```
+3. Authenticate yourself:
+    ```bash
+    graph auth https://api.thegraph.com/deploy/ <ACCESS_TOKEN>
+    ```
+3. Deploy:
+    ```bash
+    npm run deploy
+    ```
--- src/mapping.ts
+++ src/mapping.ts
@@ -7,28 +7,28 @@ import {LRCEvent,} from "../generated/schema";
 import {ethereum} from "@graphprotocol/graph-ts/chain/ethereum"
 
 export function handleLRCRewarded(event: LRCRewarded): void {
-    const lrcEvent = new LRCEvent(event.transaction.hash.toHex())
+    let lrcEvent = new LRCEvent(event.transaction.hash.toHex())
     lrcEvent.eventType = "REWARD"
     lrcEvent.amount = event.params.amount
     setPropertiesAndSave(event, lrcEvent)
 }
 
 export function handleLRCStaked(event: LRCStaked): void {
-    const lrcEvent = new LRCEvent(event.transaction.hash.toHex())
+    let lrcEvent = new LRCEvent(event.transaction.hash.toHex())
     lrcEvent.eventType = "STAKE"
     lrcEvent.amount = event.params.amount
     setPropertiesAndSave(event, lrcEvent)
 }
 
 export function handleLRCWithdrawn(event: LRCWithdrawn): void {
-    const lrcEvent = new LRCEvent(event.transaction.hash.toHex())
+    let lrcEvent = new LRCEvent(event.transaction.hash.toHex())
     lrcEvent.eventType = "WITHDRAWAL"
     lrcEvent.amount = event.params.amount
     setPropertiesAndSave(event, lrcEvent)
 }
 
 function setPropertiesAndSave(event: ethereum.Event, lrcEvent: LRCEvent): void {
-    lrcEvent.user = event.transaction.from.toString()
+    lrcEvent.user = event.transaction.from
     lrcEvent.blockNumber = event.block.number
     lrcEvent.blockTimestamp = event.block.timestamp
     lrcEvent.save()
