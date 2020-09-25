62494471d6a7cb4b707c588d3079e6d991330ac2
benesjan <janbenes1234@gmail.com>
2020-09-25 13:48:38 +0200

    Change entity name according to naming convention

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,31 +3,31 @@ import {
     LRCStaked,
     LRCWithdrawn
 } from "../generated/UserStakingPool/UserStakingPool";
-import {LRCEvent,} from "../generated/schema";
+import {LrcEvent,} from "../generated/schema";
 import {ethereum} from "@graphprotocol/graph-ts/chain/ethereum"
 
 export function handleLRCRewarded(event: LRCRewarded): void {
-    let lrcEvent = new LRCEvent(event.transaction.hash.toHex())
+    let lrcEvent = new LrcEvent(event.transaction.hash.toHex())
     lrcEvent.eventType = "REWARD"
     lrcEvent.amount = event.params.amount
     setPropertiesAndSave(event, lrcEvent)
 }
 
 export function handleLRCStaked(event: LRCStaked): void {
-    let lrcEvent = new LRCEvent(event.transaction.hash.toHex())
+    let lrcEvent = new LrcEvent(event.transaction.hash.toHex())
     lrcEvent.eventType = "STAKE"
     lrcEvent.amount = event.params.amount
     setPropertiesAndSave(event, lrcEvent)
 }
 
 export function handleLRCWithdrawn(event: LRCWithdrawn): void {
-    let lrcEvent = new LRCEvent(event.transaction.hash.toHex())
+    let lrcEvent = new LrcEvent(event.transaction.hash.toHex())
     lrcEvent.eventType = "WITHDRAWAL"
     lrcEvent.amount = event.params.amount
     setPropertiesAndSave(event, lrcEvent)
 }
 
-function setPropertiesAndSave(event: ethereum.Event, lrcEvent: LRCEvent): void {
+function setPropertiesAndSave(event: ethereum.Event, lrcEvent: LrcEvent): void {
     lrcEvent.user = event.transaction.from
     lrcEvent.blockNumber = event.block.number
     lrcEvent.blockTimestamp = event.block.timestamp
