3f3254ade0e35dfb2ef4d230dd0b8bff51a255a5
benesjan <janbenes1234@gmail.com>
2020-11-10 11:59:24 -0600

    Fixing potential id conflict

--- src/mapping.ts
+++ src/mapping.ts
@@ -7,21 +7,27 @@ import {LrcEvent,} from "../generated/schema";
 import {ethereum} from "@graphprotocol/graph-ts/chain/ethereum"
 
 export function handleLRCRewarded(event: LRCRewarded): void {
-    let lrcEvent = new LrcEvent(event.transaction.hash.toHex())
+    let id = event.transaction.hash.toHexString().concat('-').concat(event.logIndex.toString())
+    let lrcEvent = new LrcEvent(id)
+    lrcEvent.txHash = event.transaction.hash
     lrcEvent.eventType = "REWARD"
     lrcEvent.amount = event.params.amount
     setPropertiesAndSave(event, lrcEvent)
 }
 
 export function handleLRCStaked(event: LRCStaked): void {
-    let lrcEvent = new LrcEvent(event.transaction.hash.toHex())
+    let id = event.transaction.hash.toHexString().concat('-').concat(event.logIndex.toString())
+    let lrcEvent = new LrcEvent(id)
+    lrcEvent.txHash = event.transaction.hash
     lrcEvent.eventType = "STAKE"
     lrcEvent.amount = event.params.amount
     setPropertiesAndSave(event, lrcEvent)
 }
 
 export function handleLRCWithdrawn(event: LRCWithdrawn): void {
-    let lrcEvent = new LrcEvent(event.transaction.hash.toHex())
+    let id = event.transaction.hash.toHexString().concat('-').concat(event.logIndex.toString())
+    let lrcEvent = new LrcEvent(id)
+    lrcEvent.txHash = event.transaction.hash
     lrcEvent.eventType = "WITHDRAWAL"
     lrcEvent.amount = event.params.amount
     setPropertiesAndSave(event, lrcEvent)
